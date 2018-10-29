0d02052773396aed9b297bf78a7c9738a6865d26
Gustavo <guhpereira@outlook.com.br>
2018-10-29 08:33:16 -0300

    Component

--- src/components/Talks/Talks.js
+++ src/components/Talks/Talks.js
@@ -4,7 +4,15 @@ import React from 'react'
 const Talks = () =>(
     <div>
         <div className="page" id="p3">
-            <div className="icon fa fa-rocket"><span className="title">Rocket</span></div>
+            <div className="icon fa fa-keyboard-o"><span className="title"></span>
+                <div className="hint">
+
+                    <h3 className="titleDate">2017</h3>
+                    <ul className="menu-list">
+                        <li className="item-list">Introdução sobre Github</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     </div>
 )
